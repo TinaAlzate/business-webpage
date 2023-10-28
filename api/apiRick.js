0b1c039c2a694c21ab1b362ddd23a4bdd0c3b82a
Rocío <rociogalindez94@gmail.com>
2023-10-28 13:44:31 +0200

    changes cards

--- api/apiRick.js
+++ api/apiRick.js
@@ -22,14 +22,15 @@ const sectionApi = document.querySelector('.rickMortyApi');
 export function addCardCharacter(results, info){
   for (let { image, name, id, status, species, gender, episode } of results) {
     let container = `
-            <div class="card m-3 col-md-4" style="width: 18rem;" data-card="cardApi">
+            <div class="card m-3 col-md-4" style="width: 18rem;"  data-card="cardApi">
                 <img
                     src="${image}"
                     class="card-img-top"
                     alt="${name}"
                     data-img="img-card"
                 >
-                <div class="card-body">
+                <div class="card-body ${status === jsonConstantes.ALIVE ? 'alert alert-success' : 'alert alert-danger' }" 
+                    role="alert" style="width: 18rem; margin:0" >
                     <h5 class="card-title" data-name="name">${name}</h5>
                     <div
                     class="${status === jsonConstantes.ALIVE ? 'alert alert-success' : 'alert alert-danger' }"
@@ -37,7 +38,8 @@ export function addCardCharacter(results, info){
                     >
                       <p class="card-text" data-status="status">${status === jsonConstantes.ALIVE ? '✅' : '☠️' }</p>
                     </div>
-                    <p class="card-text" data-species="species">${species}</p>
+                    <p class="card-text" data-species="species">${species === jsonConstantes.HUMAN ? '👩‍🦲' :  '👽' }
+                  </p>
                     <p class="card-text" data-origin="origin">${id}</p>
                     <p class="card-text" data-gender="gender">${gender}</p>
                 </div>
@@ -49,10 +51,11 @@ export function addCardCharacter(results, info){
 
 
 const rowContainer = document.createElement('div');
-rowContainer.setAttribute('class', 'row mx-auto');
+rowContainer.setAttribute('class', 'row mx-auto' );
 
 function createTamplet(objectHtml){
   const tagMain = document.createElement('div');
+  console.log(objectHtml)
   tagMain.setAttribute('class', 'col-4 d-flex justify-content-center align-items-center');
   tagMain.innerHTML = objectHtml;
   rowContainer.append(tagMain);
