b55040041111abba11f1dafe10020852c69c1a15
Rocío <rociogalindez94@gmail.com>
2023-10-28 13:44:54 +0200

    change species

--- constantes/constantes.js
+++ constantes/constantes.js
@@ -2,5 +2,6 @@
 export const jsonConstantes = {
   API_RICK_MORTY : 'https://rickandmortyapi.com/api/character',
   API_SERVIDOR: "https://servidorsalleform-production.up.railway.app/form",
-  ALIVE: 'Alive'
+  ALIVE: 'Alive',
+  HUMAN: 'Human',
 };
